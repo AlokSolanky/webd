e4f90e2f61fcc1d33b2563fa594d3ab67cbded47
AlokSolanky <aloksolanky1@gmail.com>
2023-07-13 01:41:07 +0530

    task 3 is completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello World");
+const header = document.getElementById('main-header');
+
+header.style.borderBottom = '7px solid black';
+
+const tile = document.querySelector('.title');
+tile.style.fontWeight = 'bold';
+tile.style.color = 'yellow';
