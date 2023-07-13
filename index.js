2b875f13bea630de4adbb37c5f9f9a6a7651b89f
AlokSolanky <aloksolanky1@gmail.com>
2023-07-13 16:20:28 +0530

    task 6 part 2 is also done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const li = document.querySelector('.list-group-item:nth-child(2)');
-li.style.backgroundColor = "green";
+const secondli = document.querySelectorAll('.list-group-item:nth-child(2)');
+secondli[0].style.color = "green";
 
-const thirdLi = document.querySelector(".list-group-item:nth-child(3)");
-thirdLi.style.display = "none";
+// or
+
+// const secondli = document.querySelectorAll(".list-group-item");
+// secondli[1].style.color = "green";
+
+const oddLi = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(let i = 0;i<oddLi.length;i++)
+{
+    oddLi[i].style.backgroundColor = "green";
+}
